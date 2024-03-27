a5d499e476b3e7cfe96050e3f9ec29a8d708903e
raymond-as <a.rahman.sa16@gmail.com>
2024-03-27 11:10:13 -0400

    the backend app is added.

--- backend/src/main/java/com/knitsoftware/backend/controller/AdminController.java
+++ backend/src/main/java/com/knitsoftware/backend/controller/AdminController.java
@@ -1,6 +1,27 @@
 package com.knitsoftware.backend.controller;
 
+import com.knitsoftware.backend.entity.Admin;
+import com.knitsoftware.backend.service.AdminService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
+    private final AdminService adminService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Admin> saveAdmin(@Valid @RequestBody Admin adminRequest){
+        adminService.save(adminRequest);
+
+        return ResponseEntity.ok(adminRequest);
+    }
 
 }
